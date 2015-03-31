3e95dafeb85f3e56cd2cd46fe2eebf8b7e4ea211
Thomas Petrini <thomaslpetrini@gmail.com>
2015-03-31 10:28:22 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,11 +17,14 @@ ActiveRecord::Schema.define(version: 20150330192316) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
+
+  add_index "pin", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
